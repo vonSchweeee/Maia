00df28704fe2d346d15d1e788bd51d84051e0307
José <jvitor_2003@hotmail.com>
2020-06-14 20:49:28 -0300

    Migration nova.

--- Migrations/20200614234839_migration5.Designer.cs
+++ Migrations/20200614234839_migration5.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Maia.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Maia.Migrations
 {
     [DbContext(typeof(MaiaContext))]
-    partial class MaiaContextModelSnapshot : ModelSnapshot
+    [Migration("20200614234839_migration5")]
+    partial class migration5
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20200614234839_migration5.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Maia.Migrations
+{
+    public partial class migration5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Ativo",
+                table: "Musicas",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Ativo",
+                table: "Artistas",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Ativo",
+                table: "Albums",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ativo",
+                table: "Musicas");
+
+            migrationBuilder.DropColumn(
+                name: "Ativo",
+                table: "Artistas");
+
+            migrationBuilder.DropColumn(
+                name: "Ativo",
+                table: "Albums");
+        }
+    }
+}
